a92bd90445e3c10d2f0f268f2961d2a860e42dc3
Hsiangya <hsiangyatang@gmail.com>
2022-10-26 13:17:23 +0800

    1.4 å调整项目结构

--- application/__init__.py
+++ application/__init__.py
@@ -2,10 +2,10 @@ import os
 
 from flask import Flask
 
+from application.common.utils import setup_log
+from application.extensions import db, migrate
 from application.views import auth_bp, index_bp
-from common.utils import setup_log
 from configs import config
-from extensions import db, migrate
 
 
 def create_app(config_name=None):
@@ -20,10 +20,12 @@ def create_app(config_name=None):
 
 
 def register_extensions(app: Flask):
+    """绑定插件"""
     db.init_app(app)
     migrate.init_app(app, db)
 
 
 def register_blueprint(app: Flask):
+    """注册蓝图"""
     app.register_blueprint(index_bp)
     app.register_blueprint(auth_bp)
--- application/views/index.py
+++ application/views/index.py
@@ -1,4 +1,4 @@
-from flask import Blueprint
+from flask import Blueprint, current_app
 
 index_bp = Blueprint("index", __name__)
 
@@ -6,3 +6,9 @@ index_bp = Blueprint("index", __name__)
 @index_bp.route("/")
 def index():
     return "hello word ！"
+
+
+@index_bp.route("/favicon.ico")
+def favicon():
+    """图标展示,无法显示 待处理"""
+    return current_app.send_static_file("images/Doraemon.ico")
