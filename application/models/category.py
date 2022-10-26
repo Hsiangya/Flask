3c47e25478fb92fd907c7c1f42118b940cfa1876
Hsiangya <hsiangyatang@gmail.com>
2022-10-26 15:15:36 +0800

    1.6 分类和评论模型

---
+++ application/models/category.py
@@ -0,0 +1,10 @@
+from application import db
+
+
+class CategoryORM(db.Model):
+    """文章分类"""
+
+    __tablename__ = "info_category"
+
+    id = db.Column(db.Integer, primary_key=True)  # 分类编号
+    name = db.Column(db.String(64), nullable=False)  # 分类名
---
+++ application/models/comment.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+
+from application import db
+
+
+class CommentORM(db.Model):
+    """评论"""
+
+    __tablename__ = "info_comment"
+
+    id = db.Column(db.Integer, primary_key=True)  # 评论编号
+    content = db.Column(db.Text, nullable=False)  # 评论内容
+    like_count = db.Column(db.Integer, default=0)  # 点赞条数
+    user_id = db.Column(
+        db.Integer, db.ForeignKey("info_user.id"), nullable=False
+    )  # 用户 id
+    article_id = db.Column(
+        db.Integer, db.ForeignKey("info_article.id"), nullable=False
+    )  # 新闻 id
+
+    create_at = db.Column(db.DateTime, default=datetime.now)  # 记录的创建时间
+    update_at = db.Column(
+        db.DateTime, default=datetime.now, onupdate=datetime.now
+    )  # 记录的更新时间
+
+    parent_id = db.Column(db.Integer, db.ForeignKey("info_comment.id"))  # 父评论 id
