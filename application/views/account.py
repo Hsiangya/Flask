cad92c713d73d77b6f12172358c2fc4339543c4b
Hsiangya <hsiangyatang@gmail.com>
2022-11-07 09:02:15 +0800

    6.0 添加个人中心模板

--- application/views/account.py
+++ application/views/account.py
@@ -25,3 +25,28 @@ def account_info2():
 @account_bp.get("/account/avatar")
 def account_avatar():
     return render_template("account/user_avatar.html")
+
+
+@account_bp.get("/account/password")
+def account_password():
+    return render_template("account/user_password.html")
+
+
+@account_bp.get("/account/followed")
+def account_followed():
+    return render_template("account/followed.html")
+
+
+@account_bp.get("/account/collection")
+def account_collection():
+    return render_template("account/collection.html")
+
+
+@account_bp.get("/account/articles")
+def account_articles():
+    return render_template("account/articles.html")
+
+
+@account_bp.get("/account/release")
+def account_release():
+    return render_template("account/release.html")
